b3cf8f53066dd5159847bd8c68632184df36a01e
KavyaVR10 <168060791+KavyaVR10@users.noreply.github.com>
2025-02-17 08:21:34 +0530

    TaskD6

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -3,3 +3,28 @@ function testFunction()
     console.log("This is a test function");
 }
 testFunction()
+
+//PARAMETERIZED FUNCTION
+function mul(a,b) {
+    console.log(a*b)
+}
+mul(18,14)
+
+//PARAMETERIZED RETURN
+function sub(a,b){
+    return a-b
+    }
+    console.log(sub(10,20))
+
+//  NON-PARAMETERIZED FUNCTION
+function Hello(){
+    console.log("Good Morning!!!")
+}
+Hello();
+
+
+// NON-PARAMETERIZED RETURN
+function Return(){
+    return "This returns value without parameters"
+}
+console.log(Return());
